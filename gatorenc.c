2e27b71f7e2d295f50072f924d6d61fb819c2353
hskarachiwala <hskarachiwala@gmail.com>
2015-09-10 13:48:26 -0400

    Basic encryption on the file

--- gatorenc.c
+++ gatorenc.c
@@ -2,46 +2,99 @@
 #include<gcrypt.h>
 #include<string.h>
 
+
+char * requestPassword();
+
 int main(int argc,char *argv[])
 {
   int gcryptInitStatus;
-  char *filename;
+  char *filename,*password = NULL;
+
   char buffer[1024];
+  char output[1024];
+
   FILE *srcFile,*destFile;
+
+  gcry_cipher_hd_t handle;
+  gcry_error_t err = 0;
   
   if( !InitializeGcrypt() )
     return 0;
 
-  if (argc<2)
+  if (argc<2)                   //no filename given
   {
     printf("Missing file name to encrypt\n");
     return 0;
   }
 
-  filename = argv[1];
-  srcFile = fopen(filename,"r");
-  if(srcFile!=NULL)
-  {
-    do
-    {
-      fgets(buffer,1024,srcFile);
-    }while( !feof(srcFile) );
 
-    printf("%s",buffer);
-    fclose(srcFile);
+  filename = argv[1];
+  srcFile = fopen(filename,"r");      //open file
+  
+  if(srcFile == NULL)           //file not found
+  {
+    printf("File not found");
+    return 0;
   }
   else
   {
-    printf("File not found");
-  }
-
-  if(buffer[0]!='\n')
-  {
-    destFile = fopen(strcat(filename,".uf"),"w");
-    fputs(buffer,destFile);
-    fclose(destFile);
-  }
+    destFile = fopen(strcat(filename,".uf"),"w");     
+    password = requestPassword();   //get password 16 characters
     
+    do
+    {
+      fgets(buffer,1024,srcFile);   //pick from file
+
+
+      err = gcry_cipher_open (&handle, GCRY_CIPHER_AES, GCRY_CIPHER_MODE_CBC,0);    //create handle
+  
+      if (err)
+      {
+        printf ("Failure: %s%s\n",gcry_strsource (err),gcry_strerror (err));
+        printf ("Failure: %s%s\n",gcry_strsource (err),gcry_strerror (err));
+      }
+
+      err = gcry_cipher_setkey (handle, password,128);          //set key to encrypt
+      if (err)
+      {
+        printf ("Failure: %s%s\n",gcry_strsource (err),gcry_strerror (err));
+        printf ("Failure: %s%s\n",gcry_strsource (err),gcry_strerror (err));
+      }
+
+      err =  gcry_cipher_encrypt (handle,output,1024,buffer,1024);    //encrypt
+      if (err)
+      {
+        printf ("Failure: %s%s\n",gcry_strsource (err),gcry_strerror (err));
+        printf ("Failure: %s%s\n",gcry_strsource (err),gcry_strerror (err));
+      }
+  
+      printf(" out :%s ||| plain text %s\n",output, buffer);
+
+      fputs(output,destFile);    //write to encrypted file
+    }while( !feof(srcFile) );
+
+    printf("%s",buffer);
+
+    fclose(destFile);   //close both files
+    fclose(srcFile);
+  }
+  
+    
+}
+
+char * requestPassword()
+{
+  int length;
+  
+  char *pwd = (char *)malloc(16);
+
+  do
+  {
+    printf("Enter your pasword - 16 characters");
+    scanf("%s",pwd);
+  }while(strlen(pwd) > 16);
+
+  return pwd;
 }
 
 int InitializeGcrypt()
