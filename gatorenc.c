8c658d6efbc9674c6aba3c3aa432b08053428f78
hskarachiwala <hskarachiwala@gmail.com>
2015-09-10 23:24:27 -0400

    Code rearranged, key derivation needs to be fixed

--- gatorenc.c
+++ gatorenc.c
@@ -4,96 +4,105 @@
 
 
 char * requestPassword();
+void cleanup(FILE *srcFile,FILE * destFile,gcry_cipher_hd_t handle,unsigned char * plainText,unsigned char * cipherText);
 
 int main(int argc,char *argv[])
 {
-  int gcryptInitStatus;
+  int gcryptInitStatus,currentAlgoBlockSize;
   char *filename,*password = NULL;
 
-  char buffer[1024];
-  char output[1024];
+  unsigned char *plainText,*cipherText;
+  char keybuffer[256];
 
   FILE *srcFile,*destFile;
 
   gcry_cipher_hd_t handle;
-  gcry_error_t err = 0;
-  
-  if( !InitializeGcrypt() )
-    return 0;
+  gcry_error_t libgcryptError = 0;
 
   if (argc<2)                   //no filename given
   {
     printf("Missing file name to encrypt\n");
-    return 0;
+    return -1;
   }
-
 
   filename = argv[1];
   srcFile = fopen(filename,"r");      //open file
-  
   if(srcFile == NULL)           //file not found
   {
     printf("File not found");
-    return 0;
+    return -1;
   }
-  else
+
+  if( !InitializeGcrypt() )     //initializing gcrypt
   {
-    destFile = fopen(strcat(filename,".uf"),"w");     
-    password = requestPassword();   //get password 16 characters
+    printf("Failed to InitializeGcrypt");
+    return -1;
+  }
+  
+  plainText = (unsigned char*)malloc(sizeof(unsigned char) * 16);     //read 128 bits at a time
+  cipherText = (unsigned char*)malloc(sizeof(unsigned char) * 16);     
+  
+  libgcryptError = gcry_cipher_open (&handle, GCRY_CIPHER_AES256, GCRY_CIPHER_MODE_CBC,0);    //create handle
+  if (libgcryptError)
+  {
+    printf ("Failure in creating encryption handle: Details - %s\n", gcry_strerror (libgcryptError));
+    return -1;
+  }
+
+  password = requestPassword();   
     
-    do
+  libgcryptError = gcry_kdf_derive(password,strlen(password),GCRY_KDF_PBKDF2 ,GCRY_MD_SHA256, "somesalt" , 8 , 100, 32 ,keybuffer );
+  if (libgcryptError)
+  {
+    printf ("Failure in deriving key: Details -  %s\n",gcry_strerror (libgcryptError));
+    return -1;
+  }
+
+  libgcryptError = gcry_cipher_setkey (handle, keybuffer,256);          //set key to encrypt
+  if (libgcryptError)
+  {
+    printf ("Failure in setting key: Details -  %s\n",gcry_strerror (libgcryptError));
+    return -1;
+  }
+
+  destFile = fopen(strcat(filename,".uf"),"w");     
+  currentAlgoBlockSize = sizeof(gcry_cipher_get_algo_blklen(GCRY_CIPHER_AES256));  // get the block size of the algo
+  
+  while(!feof(srcFile))
+  {
+    fread(plainText,currentAlgoBlockSize,1,srcFile);    
+     
+    libgcryptError =  gcry_cipher_encrypt (handle,cipherText,currentAlgoBlockSize,plainText,currentAlgoBlockSize);    //encrypt
+    if (libgcryptError)
     {
-      fgets(buffer,1024,srcFile);   //pick from file
+      printf ("Failure in encrypting : Details -  %s\n",gcry_strerror (libgcryptError));
+      return -1;
+    }
 
+    printf(" Plaintext was : %s\n\n  Ciphertext is : %s\n\n",plainText, cipherText);
 
-      err = gcry_cipher_open (&handle, GCRY_CIPHER_AES, GCRY_CIPHER_MODE_CBC,0);    //create handle
-  
-      if (err)
-      {
-        printf ("Failure: %s%s\n",gcry_strsource (err),gcry_strerror (err));
-        printf ("Failure: %s%s\n",gcry_strsource (err),gcry_strerror (err));
-      }
+    fwrite(cipherText,currentAlgoBlockSize,1,destFile);    //write to encrypted file
 
-      err = gcry_cipher_setkey (handle, password,128);          //set key to encrypt
-      if (err)
-      {
-        printf ("Failure: %s%s\n",gcry_strsource (err),gcry_strerror (err));
-        printf ("Failure: %s%s\n",gcry_strsource (err),gcry_strerror (err));
-      }
+  }
 
-      err =  gcry_cipher_encrypt (handle,output,1024,buffer,1024);    //encrypt
-      if (err)
-      {
-        printf ("Failure: %s%s\n",gcry_strsource (err),gcry_strerror (err));
-        printf ("Failure: %s%s\n",gcry_strsource (err),gcry_strerror (err));
-      }
-  
-      printf(" out :%s ||| plain text %s\n",output, buffer);
+  cleanup(srcFile,destFile,handle,plainText,cipherText);       
+    
+}
 
-      fputs(output,destFile);    //write to encrypted file
-    }while( !feof(srcFile) );
-
-    printf("%s",buffer);
-
+void cleanup(FILE *srcFile,FILE * destFile,gcry_cipher_hd_t handle,unsigned char * plainText,unsigned char * cipherText)
+{
     fclose(destFile);   //close both files
     fclose(srcFile);
-  }
-  
-    
+    gcry_cipher_close(handle);
+    free(plainText);
+    free(cipherText);
 }
 
 char * requestPassword()
 {
-  int length;
-  
-  char *pwd = (char *)malloc(16);
-
-  do
-  {
-    printf("Enter your pasword - 16 characters");
-    scanf("%s",pwd);
-  }while(strlen(pwd) > 16);
-
+  char *pwd = (char *)malloc(1024);
+  printf("\nEnter the password \t");
+  scanf("%s",pwd);
   return pwd;
 }
 
@@ -101,8 +110,8 @@ int InitializeGcrypt()
 {
   if (!gcry_check_version (GCRYPT_VERSION))               //version check
   {
-    fputs ("libgcrypt version mismatch\n", stderr);
-    return 0;
+    printf("Version Error\n");
+    return -1;
   }
   gcry_control (GCRYCTL_SUSPEND_SECMEM_WARN);           // suspend warnings
   gcry_control (GCRYCTL_INIT_SECMEM, 16384, 0);         // allocate secure memory
